4cf88cca9610587cbbaa28b92d221cfa6d642143
danieljin <daniel.jin@dfns.co>
2025-10-30 20:41:54 -0400

    sync data models

--- packages/sdk/generated/wallets/delegatedClient.ts
+++ packages/sdk/generated/wallets/delegatedClient.ts
@@ -50,6 +50,49 @@ export class DelegatedWalletsClient {
     return response.json()
   }
 
+  async activateWalletInit(request: T.ActivateWalletRequest): Promise<UserActionChallengeResponse> {
+    const path = buildPathAndQuery('/wallets/:walletId/activate', {
+      path: request ?? {},
+      query: {},
+    })
+
+    const challenge = await BaseAuthApi.createUserActionChallenge(
+      {
+        userActionHttpMethod: 'POST',
+        userActionHttpPath: path,
+        userActionPayload: JSON.stringify(request.body),
+        userActionServerKind: 'Api',
+      },
+      this.apiOptions
+    )
+
+    return challenge
+  }
+
+  async activateWalletComplete(
+    request: T.ActivateWalletRequest,
+    signedChallenge: SignUserActionChallengeRequest
+  ): Promise<T.ActivateWalletResponse> {
+    const path = buildPathAndQuery('/wallets/:walletId/activate', {
+      path: request ?? {},
+      query: {},
+    })
+
+    const { userAction } = await BaseAuthApi.signUserActionChallenge(
+      signedChallenge,
+      this.apiOptions
+    )
+
+    const response = await simpleFetch(path, {
+      method: 'POST',
+      body: request.body,
+      headers: { 'x-dfns-useraction': userAction },
+      apiOptions: this.apiOptions,
+    })
+
+    return response.json()
+  }
+
   async broadcastTransactionInit(request: T.BroadcastTransactionRequest): Promise<UserActionChallengeResponse> {
     const path = buildPathAndQuery('/wallets/:walletId/transactions', {
       path: request ?? {},
