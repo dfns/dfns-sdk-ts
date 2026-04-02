24fa00340a05fb0e9d42f9952712603ab5718be8
Xavier Bruni <xavier@dfns.co>
2026-04-02 15:19:18 +0200

    ci: switch to manual release

---
+++ scripts/publish.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euo pipefail
+
+npm run cb:all
+
+packages=(
+    "@dfns/lib-algorand"
+    "@dfns/lib-aptos"
+    "@dfns/lib-bitcoinjs"
+    "@dfns/lib-concordium"
+    "@dfns/lib-cosmjs"
+    "@dfns/lib-ethersjs5"
+    "@dfns/lib-ethersjs6"
+    "@dfns/lib-hedera"
+    "@dfns/lib-iota"
+    "@dfns/lib-kaspa"
+    "@dfns/lib-meshsdk"
+    "@dfns/lib-near"
+    "@dfns/lib-polkadot"
+    "@dfns/lib-polymesh"
+    "@dfns/lib-solana"
+    "@dfns/lib-stellar"
+    "@dfns/lib-sui"
+    "@dfns/lib-taquito"
+    "@dfns/lib-ton"
+    "@dfns/lib-tron"
+    "@dfns/lib-vechain"
+    "@dfns/lib-viem"
+    "@dfns/lib-xrpl"
+    "@dfns/sdk"
+    "@dfns/sdk-awskmssigner"
+    "@dfns/sdk-browser"
+    "@dfns/sdk-keyexport-utils-nodejs"
+    "@dfns/sdk-keyexport-utils-bundler"
+    "@dfns/sdk-keyimport-utils-nodejs"
+    "@dfns/sdk-keyimport-utils-bundler"
+    "@dfns/sdk-keysigner"
+    "@dfns/sdk-react-native"
+)
+
+for packageName in "${packages[@]}"; do
+    cd dist/"${packageName}"
+    echo "Publishing ${packageName}..."
+    npm publish --workspaces=false
+    cd - >/dev/null
+done
